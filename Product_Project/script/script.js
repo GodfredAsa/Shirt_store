097b737fd9bbd2a71930a72b8be7398c3b59ab7e
GodfredAsa <77088001+GodfredAsa@users.noreply.github.com>
2021-04-13 22:02:56 +0000

    final js file commit

--- Product_Project/script/script.js
+++ Product_Project/script/script.js
@@ -1,33 +1,41 @@
+
+
+
 //CLASS FOR DATA
+
+//	console.log(deleteCartElement);
+
+//DELETING AN ITEM IN A CART
 
 //basic class fields of products
 class Product{
-constructor(title, image, price, desc){
+constructor(title, image, price, desc,stock){
 	this.title = title;
 	this.imageUrl = image;
 	this.price = price;
-	this.description = desc; }
+	this.description = desc;
+	this.stock = stock;
+}
 
 }
 
 //class for handling item selected
 class ShoppingCart{
-	
 	items = [];
 
 addProduct(product){
 	this.items.push(product);
-	this.totalOutput =`<h2>\$ ${0}</h2>`;
-	 
+	this.totalOutput =`<h2>\$ ${0}</h2>`; 
 }
 //creating a button for adding to cart
 	render(){
+		
 	const cartEl = document.createElement('section');
-		cartEl.innerHTML = 
-			`
-			<h2>Total: \$ ${0} </h2>
-			<button> Order Now </button>
-			`;
+//		cartEl.innerHTML = 
+//			`
+//			<h2>Total: \$ ${0} </h2>
+//			<button> Order Now </button>
+//			`;
 		cartEl.className = 'cart';
 		this.totalOutput = cartEl.querySelector('h2');
 		return cartEl;
@@ -46,24 +54,22 @@ class ProductItem{
 		this.product = product;
 	}
 //	button evenListener function
-	addToCart() {
-	//	alert('Adding to cart');
-//		alert(this.product);
-		
+	addToCart() {	
 	}
-	
 	render(){
+	
 		const prodEl = document.createElement('li');
 			prodEl.className = 'product-grid';
 			prodEl.innerHTML = 
 				`<div class="one-item">
-					<img src = "${this.product.imageUrl}" alt="${this.product.title}">
+					<img src = "${this.product.imageUrl}" alt="${this.product.title}" class="i">
 					<div>
 						<h2> ${this.product.title}</h2>
-						<h3 class="price">\$ ${this.product.price}</h3>
+						<h3 class="price">GHC ${this.product.price}</h3>
 						<p> ${this.product.description}</p>
-						<button class="add-cart"> + Cart</button>
-						<button class="add-cart cart-delete">Delete</button>
+						<h3 class="stock"> Stock: ${this.product.stock}</h3>
+						<button class="add-cart"> Add to Cart</button>
+						<button class="cart-delete"> Delete</button>
 					</div>
 				 </div>`;
 		
@@ -78,7 +84,7 @@ class ProductItem{
 
 		let addBtn = this;
 		let removeBtn = document.createElement('button');
-		removeBtn.textContent = 'Remove';
+		removeBtn.textContent = 'Remove from Cart';
 		removeBtn.className = 'remove-cart';
 		removeBtn.onclick = function () {
 			// num =  numberOfItemInCart--;
@@ -99,12 +105,12 @@ class ProductItem{
 class ProductList{
 //	instances of the Products, 6 instances in total
 	products = [
-		new Product('Dam Brown', '../images/shirt2.jpg', 90.89,'A smooth and slim fit cotton wear'),
-		new Product('Easy Wear', '../images/shirt6.jpg', 50.40,'Look gently fit'),
-		new Product('White Boxed sleeve', '../images/shirt4.jpg', 40.69,'Feel as a King'),
-		new Product('Jean Long Sleeve', '../images/shirt9.jpg', 120.60,'Fit as a Fiddle'),
-		new Product('Blue sleeve Shirt', '../images/shirt8.jpg', 30.69,'A smooth slim fit sleeve'),
-		new Product('G-Shirt', '../images/shirt5.jpg', 40.69,'Simply classic fit')
+		new Product('Dam Brown', '../images/shirt2.jpg', 90.89,'A smooth and slim fit cotton wear', 3),
+		new Product('Easy Wear', '../images/shirt6.jpg', 50.40,'Look gently fit', 2),
+		new Product('White Boxed sleeve', '../images/shirt4.jpg', 40.69,'Feel as a King', 1),
+		new Product('Jean Long Sleeve', '../images/shirt9.jpg', 120.60,'Fit as a Fiddle', 3),
+		new Product('Blue sleeve Shirt', '../images/shirt8.jpg', 30.69,'A smooth slim fit sleeve', 4),
+		new Product('G-Shirt', '../images/shirt5.jpg', 40.69,'Simply classic fit',2)
 		];
 
 constructor(){}
@@ -125,6 +131,9 @@ render(){
 }
 
 class Shop{
+//	navBar creation
+		
+	//}
 	render(){
 		const productHandler = document.querySelector('.products');
 		const cart = new ShoppingCart();
@@ -140,4 +149,15 @@ class Shop{
 const shop  =  new Shop();
 shop.render();
 
+
+let cartDeleteHandler = function(){
+	let itemList = document.querySelectorAll('.cart-delete');
+	for(let index = 0; index<itemList.length;index++){
+		itemList[index].addEventListener('click', function(){
+			this.closest('li').classList.toggle('invisible');
+		});
+	}
+}
+
+cartDeleteHandler();
 
